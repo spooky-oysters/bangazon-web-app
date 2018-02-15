fad97962cd46cd92f8e8854d2509f84b144a220a
Dre Randaci <drerandaci@gmail.com>
2018-02-14 19:19:21 -0800

    setup files. run program in visual studio to test that template website is running

---
+++ bangazon-web-app-spooy-oysters/bangazon-web-app-spooy-oysters/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace bangazon_web_app_spooy_oysters.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
