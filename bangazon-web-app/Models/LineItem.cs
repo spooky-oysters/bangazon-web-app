6701836f0d6d17916f4d6fcb4e2fb5948baf7c92
ghostbike <krysmathis@gmail.com>
2018-02-16 06:09:33 -0600

    updated Order Model to include CreationDate

--- bangazon-web-app/Models/LineItem.cs
+++ bangazon-web-app/Models/LineItem.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace Bangazon.Models
 {
     public class LineItem
     {
+        [Key]
+        public int OrderProductId { get; set; }
+
+        [Required]
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
     }
 }
