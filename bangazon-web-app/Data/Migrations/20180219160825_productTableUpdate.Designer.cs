fbfd2e211df308f997d849a4f291d54737f08c99
Dre Randaci <drerandaci@gmail.com>
2018-02-19 10:11:21 -0600

    added migration for product model updates

--- bangazon-web-app/Data/Migrations/20180219160825_productTableUpdate.Designer.cs
+++ bangazon-web-app/Data/Migrations/20180219160825_productTableUpdate.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Bangazon.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180219160825_productTableUpdate")]
+    partial class productTableUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ bangazon-web-app/Data/Migrations/20180219160825_productTableUpdate.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Bangazon.Data.Migrations
+{
+    public partial class productTableUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Product",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Product",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "LocalDelivery",
+                table: "Product",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Product");
+
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Product");
+
+            migrationBuilder.DropColumn(
+                name: "LocalDelivery",
+                table: "Product");
+        }
+    }
+}
