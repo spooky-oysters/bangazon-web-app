6412f20ee5b9ea2a006022be63e3ab7846e28081
KimberlyJBird <kimberly.j.bird@gmail.com>
2018-02-19 15:59:19 -0600

    added selling product to nav bar based on authentication

--- bangazon-web-app/wwwroot/js/site.js
+++ bangazon-web-app/wwwroot/js/site.js
@@ -1,1 +1,8 @@
 ﻿// Write your JavaScript code.
+
+// function to remove "hidden" class on div. When "local delivery" clicked, "city" field appears
+document.getElementById("localDeliveryCheck").addEventListener("click", function () {
+    console.log("button clicked")
+    document.getElementsByClassName("hidden").remove()
+  
+})
