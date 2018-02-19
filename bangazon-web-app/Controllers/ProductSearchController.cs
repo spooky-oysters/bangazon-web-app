ffceb82cb785d5850f6c6e018acc3af5b592b399
John <john.s.dulaney1@gmail.com>
2018-02-19 08:18:24 -0800

    pre merge

---
+++ bangazon-web-app/Controllers/ProductSearchController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Bangazon.Models;
+using Bangazon.Models.ManageViewModels;
+using Bangazon.Services;
+
+namespace Bangazon.Controllers
+{
+    public class ProductSearchController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Search()
+        {
+            ViewData["Message"] = "Search for a product.";
+
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
