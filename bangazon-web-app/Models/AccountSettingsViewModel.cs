276342275527c16aa302f013eda90edac6933cc6
Dre Randaci <drerandaci@gmail.com>
2018-02-20 14:59:18 -0600

    my account nav bar link

---
+++ bangazon-web-app/Models/AccountSettingsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Bangazon.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon.Models
+{
+    public class AccountSettingsViewModel
+    {
+        public ApplicationUser User { get; set; }
+
+        public ICollection<PaymentType> PaymentOptions { get; set; }
+        public ICollection<Order> OrderHistory { get; set; }
+
+        public AccountSettingsViewModel(ApplicationDbContext ctx)
+        {
+
+        }
+    }
+}
