ae7634ffbc8292ee86a2183afd1147b82ea671ff
krysmathis <krysmathis@gmail.com>
2018-02-20 10:16:26 -0600

    created OrderController with scaffolding

---
+++ bangazon-web-app/Models/OrderViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon.Models.OrderViewModels
+{
+    public class ShoppingCartViewModel
+    {
+    }
+}
