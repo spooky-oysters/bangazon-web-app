19e672d3af29ed46d5e86ae4561ac0293a532570
krysmathis <krysmathis@gmail.com>
2018-02-21 13:19:31 -0600

    made total lines currency

--- bangazon-web-app/Models/OrderViewModels/ShoppingCartLineItemViewModel.cs
+++ bangazon-web-app/Models/OrderViewModels/ShoppingCartLineItemViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,8 @@ namespace Bangazon.Models.OrderViewModels
     {
         public string ProductName { get; set; }
         public int Units { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C}")]
         public double Total { get; set; }
         
     }
