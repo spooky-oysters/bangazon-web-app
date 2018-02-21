d86352dc26c764d3e21b30c4df4c27a7cef840b5
John <john.s.dulaney1@gmail.com>
2018-02-21 13:07:19 -0800

    added viewmodel structure, working ticket 18, fixed navbar

--- bangazon-web-app/Models/ProductViewModels/ProductListViewModel.cs
+++ bangazon-web-app/Models/ProductViewModels/ProductListViewModel.cs
@@ -1,33 +1,14 @@
-﻿//using System.Collections.Generic;
-//using System.Linq;
-//using Bangazon.Data;
-//using Microsoft.AspNetCore.Mvc.Rendering;
-//using System;
+﻿using Bangazon.Data;
+using Bangazon.Models;
+using System.Collections.Generic;
 
-//namespace Bangazon.Models.ProductViewModels
-//{
-//    public class ProductListViewModel
-//    {
-//        //public List<SelectListItem> ProductTypeId { get; set; }
-//        public Product Product { get; set; }
-//        public ProductListViewModel(ApplicationDbContext ctx)
-//        {
-//            // Creating SelectListItems will be used in a @Html.DropDownList
-//            // control in a Razor template. 
-//            this.ProductTypeId = ctx.ProductType
-//                .OrderBy(l => l.Label)
-//                .AsEnumerable()
-//                .Select(li => new SelectListItem
-//                {
-//                    Text = li.Label,
-//                    Value = li.ProductTypeId.ToString()
-//                }).ToList();
-
-//            this.ProductTypeId.Insert(0, new SelectListItem
-//            {
-//                Text = "Choose category...",
-//                Value = "0"
-//            });
-//        }
-//    }
-//}
+namespace bangazon.models.ProductViewModels
+{
+    public class ProductListViewModel
+    {
+        public Product product { get; set; }
+        public IEnumerable<ProductListViewModel> TopProduct { get; set; }
+        //public productlistviewmodel(ApplicationDbContext ctx)
+        
+    }
+}
