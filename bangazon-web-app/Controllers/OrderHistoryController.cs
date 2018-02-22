5a1116611e5a31dda9c462968e7fb90d5b7ad8bf
Dre Randaci <drerandaci@gmail.com>
2018-02-22 14:24:02 -0600

    users can view order details with order total and all products associated

--- bangazon-web-app/Controllers/OrderHistoryController.cs
+++ bangazon-web-app/Controllers/OrderHistoryController.cs
@@ -38,7 +38,7 @@ namespace Bangazon.Controllers
             // Grab the currently logged in user
             var user = await GetCurrentUserAsync();
 
-            // Queries for orders that have been completed and includes all the payment types associated with the orders
+            // Queries for orders that have been completed and includes all the payment types associated with each order
             model.Orders = await _context
                 .Order
                 .Include("PaymentType")
@@ -51,22 +51,35 @@ namespace Bangazon.Controllers
         // GET: OrderHistory/Details/5
         public async Task<IActionResult> Details(int id)
         {
+            // Instantiates an instance of the ViewModel
             OrderHistoryDetailViewModel model = new OrderHistoryDetailViewModel();
 
-            model.Order = await _context
+            try
+            {
+                // Queries for the requested order
+                model.Order = await _context
                  .Order
                  .SingleOrDefaultAsync(o => o.OrderId == id);
 
-            model.Products = await _context
-                 .LineItem
-                 .Include("Product")
-                 .Where(l => l.OrderId == id)
-                 .ToListAsync();
+                // Queries for all the products associated with that order
+                model.Products = await _context
+                     .LineItem
+                     .Include("Product")
+                     .Where(l => l.OrderId == id)
+                     .ToListAsync();
+            } catch
+            {
+                throw;
+            }
+                        
 
             if (model.Products == null)
             {
                 return NotFound();
             }
+
+            // Total for the requested order
+            model.OrderTotal = model.Products.Sum(p => p.Product.Price);
 
             return View(model);
         }
--- bangazon-web-app/Models/OrderHistoryViewModels/OrderHistoryDetailViewModel.cs
+++ bangazon-web-app/Models/OrderHistoryViewModels/OrderHistoryDetailViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,14 @@ namespace Bangazon.Models.OrderHistoryViewModels
 {
     public class OrderHistoryDetailViewModel
     {
+        [Required]
         public IEnumerable<LineItem> Products { get; set; }
+
+        [Required]
         public Order Order { get; set; }
-        public double Total { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public double OrderTotal { get; set; }
     }
 }
